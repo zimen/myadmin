33b1cfeb7557241c93626e458ce4c4f25320e8bf
Didier Simen <cedric.ngaha@sprint-pay.coml>
2019-12-24 15:02:10 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,163 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgReduxModule} from '@angular-redux/store';
+import {NgRedux, DevToolsExtension} from '@angular-redux/store';
+import {rootReducer, ArchitectUIState} from './ThemeOptions/store';
+import {ConfigActions} from './ThemeOptions/store/config.actions';
+import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+
+// BOOTSTRAP COMPONENTS
+
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {PERFECT_SCROLLBAR_CONFIG} from 'ngx-perfect-scrollbar';
+import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
+import {ChartsModule} from 'ng2-charts';
+
+// LAYOUT
+
+import {BaseLayoutComponent} from './Layout/base-layout/base-layout.component';
+import {PagesLayoutComponent} from './Layout/pages-layout/pages-layout.component';
+import {PageTitleComponent} from './Layout/Components/page-title/page-title.component';
+
+// HEADER
+
+import {HeaderComponent} from './Layout/Components/header/header.component';
+import {SearchBoxComponent} from './Layout/Components/header/elements/search-box/search-box.component';
+import {UserBoxComponent} from './Layout/Components/header/elements/user-box/user-box.component';
+
+// SIDEBAR
+
+import {SidebarComponent} from './Layout/Components/sidebar/sidebar.component';
+import {LogoComponent} from './Layout/Components/sidebar/elements/logo/logo.component';
+
+// FOOTER
+
+import {FooterComponent} from './Layout/Components/footer/footer.component';
+// DEMO PAGES
+
+// Dashboards
+
+import {AnalyticsComponent} from './DemoPages/Dashboards/analytics/analytics.component';
+
+// Pages
+
+import {ForgotPasswordBoxedComponent} from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
+import {LoginBoxedComponent} from './DemoPages/UserPages/login-boxed/login-boxed.component';
+import {RegisterBoxedComponent} from './DemoPages/UserPages/register-boxed/register-boxed.component';
+
+// Elements
+
+import {StandardComponent} from './DemoPages/Elements/Buttons/standard/standard.component';
+import {DropdownsComponent} from './DemoPages/Elements/dropdowns/dropdowns.component';
+import {CardsComponent} from './DemoPages/Elements/cards/cards.component';
+import {ListGroupsComponent} from './DemoPages/Elements/list-groups/list-groups.component';
+import {TimelineComponent} from './DemoPages/Elements/timeline/timeline.component';
+import {IconsComponent} from './DemoPages/Elements/icons/icons.component';
+
+// Components
+
+import {AccordionsComponent} from './DemoPages/Components/accordions/accordions.component';
+import {TabsComponent} from './DemoPages/Components/tabs/tabs.component';
+import {CarouselComponent} from './DemoPages/Components/carousel/carousel.component';
+import {ModalsComponent} from './DemoPages/Components/modals/modals.component';
+import {ProgressBarComponent} from './DemoPages/Components/progress-bar/progress-bar.component';
+import {PaginationComponent} from './DemoPages/Components/pagination/pagination.component';
+import {TooltipsPopoversComponent} from './DemoPages/Components/tooltips-popovers/tooltips-popovers.component';
+
+// Tables
+
+import {RegularComponent} from './DemoPages/Tables/regular/regular.component';
+import {TablesMainComponent} from './DemoPages/Tables/tables-main/tables-main.component';
+
+// Widgets
+
+import {ChartBoxes3Component} from './DemoPages/Widgets/chart-boxes3/chart-boxes3.component';
+
+// Forms Elements
+
+import {ControlsComponent} from './DemoPages/Forms/Elements/controls/controls.component';
+import {LayoutComponent} from './DemoPages/Forms/Elements/layout/layout.component';
+
+// Charts
+
+import {ChartjsComponent} from './DemoPages/Charts/chartjs/chartjs.component';
+
+// Chart.js Examples
+
+//import {LineChartComponent} from './DemoPages/Charts/chartjs/examples/line-chart/line-chart.component';
+import {BarChartComponent} from './DemoPages/Charts/chartjs/examples/bar-chart/bar-chart.component';
+import {ScatterChartComponent} from './DemoPages/Charts/chartjs/examples/scatter-chart/scatter-chart.component';
+import {RadarChartComponent} from './DemoPages/Charts/chartjs/examples/radar-chart/radar-chart.component';
+import {PolarAreaChartComponent} from './DemoPages/Charts/chartjs/examples/polar-area-chart/polar-area-chart.component';
+import {BubbleChartComponent} from './DemoPages/Charts/chartjs/examples/bubble-chart/bubble-chart.component';
+import {DynamicChartComponent} from './DemoPages/Charts/chartjs/examples/dynamic-chart/dynamic-chart.component';
+import {DoughnutChartComponent} from './DemoPages/Charts/chartjs/examples/doughnut-chart/doughnut-chart.component';
+import {PieChartComponent} from './DemoPages/Charts/chartjs/examples/pie-chart/pie-chart.component';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+      BrowserAnimationsModule,
+      NgReduxModule,
+      LoadingBarRouterModule,
+      PerfectScrollbarModule,
+      NgbModule,
+      AngularFontAwesomeModule,
+      FormsModule,
+      // Charts
+
+      ChartsModule,
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+      // HEADER
+
+      HeaderComponent,
+      SearchBoxComponent,
+      UserBoxComponent,
+      // SIDEBAR
+
+      SidebarComponent,
+      LogoComponent,
+
+      // FOOTER
+
+      FooterComponent,
+      // DEMO PAGES
+
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,64 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { AuthGuard } from './_helpers';
+
+import {BaseLayoutComponent} from './Layout/base-layout/base-layout.component';
+import {PagesLayoutComponent} from './Layout/pages-layout/pages-layout.component';
+
+// DEMO PAGES
+
+// Dashboards
+
+import {AnalyticsComponent} from './DemoPages/Dashboards/analytics/analytics.component';
+
+// Pages
+
+import {ForgotPasswordBoxedComponent} from './DemoPages/UserPages/forgot-password-boxed/forgot-password-boxed.component';
+import {LoginBoxedComponent} from './DemoPages/UserPages/login-boxed/login-boxed.component';
+import {RegisterBoxedComponent} from './DemoPages/UserPages/register-boxed/register-boxed.component';
+
+// Elements
+
+import {StandardComponent} from './DemoPages/Elements/Buttons/standard/standard.component';
+import {DropdownsComponent} from './DemoPages/Elements/dropdowns/dropdowns.component';
+import {CardsComponent} from './DemoPages/Elements/cards/cards.component';
+import {ListGroupsComponent} from './DemoPages/Elements/list-groups/list-groups.component';
+import {TimelineComponent} from './DemoPages/Elements/timeline/timeline.component';
+import {IconsComponent} from './DemoPages/Elements/icons/icons.component';
+
+// Components
+
+import {AccordionsComponent} from './DemoPages/Components/accordions/accordions.component';
+import {TabsComponent} from './DemoPages/Components/tabs/tabs.component';
+import {CarouselComponent} from './DemoPages/Components/carousel/carousel.component';
+import {ModalsComponent} from './DemoPages/Components/modals/modals.component';
+import {ProgressBarComponent} from './DemoPages/Components/progress-bar/progress-bar.component';
+import {PaginationComponent} from './DemoPages/Components/pagination/pagination.component';
+import {TooltipsPopoversComponent} from './DemoPages/Components/tooltips-popovers/tooltips-popovers.component';
+
+// Tables
+
+import {TablesMainComponent} from './DemoPages/Tables/tables-main/tables-main.component';
+
+// Widgets
+
+import {ChartBoxes3Component} from './DemoPages/Widgets/chart-boxes3/chart-boxes3.component';
+
+// Forms Elements
+
+import {ControlsComponent} from './DemoPages/Forms/Elements/controls/controls.component';
+import {LayoutComponent} from './DemoPages/Forms/Elements/layout/layout.component';
+
+// Charts
+
+import {ChartjsComponent} from './DemoPages/Charts/chartjs/chartjs.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+export const appRoutingModule = RouterModule.forRoot(routes);
